9f6a986b63f846ac566def15bb04386ea0e7134b
robertcsapo <robertcsapo@users.noreply.github.com>
2022-05-16 17:53:58 +0000

    from dev to publish

--- examples/modular/main.tf
+++ examples/modular/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     ciscoevpn = {
-      source = "terraform.local/local/ciscoevpn"
-      //version = "0.0.1"
+      source = "robertcsapo/ciscoevpn"
+      version = "1.0.0"
     }
   }
 }
